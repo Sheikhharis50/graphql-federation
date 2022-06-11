5e8f218c7e5ea7d69a68884e64ec8574695bfbd4
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2022-06-12 01:13:16 +0500

    Sql viewing support is enabled

--- product-service/app_product/types.py
+++ product-service/app_product/types.py
@@ -1,4 +1,7 @@
 import strawberry
+from .services import ProductService
+
+product_service = ProductService()
 
 
 @strawberry.type
@@ -13,3 +16,15 @@ class ProductType:
     name: str
     description: str
     category: CategoryType
+
+    @classmethod
+    def resolve_reference(cls, id: strawberry.ID):
+        # here we could fetch the book from the database
+        # or even from an API
+        product = product_service.get_by_id(id)
+        return ProductType(
+            id=id,
+            name=product.name,
+            description=product.description,
+            category=product.category,
+        )
